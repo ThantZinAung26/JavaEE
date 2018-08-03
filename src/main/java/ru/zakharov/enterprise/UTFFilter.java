66f5a16edb3c5ea067dd181f8bc8cd720b1cf028
lexaz777 <vh1@mail.ru>
2018-08-03 14:45:01 +0300

    Домашнее задание к уроку 2.

---
+++ src/main/java/ru/zakharov/enterprise/UTFFilter.java
@@ -0,0 +1,37 @@
+package ru.zakharov.enterprise;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class UTFFilter implements Filter {
+    private String encoding;
+
+    private boolean forceEncoding;
+
+
+    public final void init(FilterConfig filterConfig) throws ServletException {
+        String encod = filterConfig.getInitParameter("encoding");
+        if(encod !=null){
+            encoding = encod;
+        }
+        String forceEncod = filterConfig.getInitParameter("forceEncoding");
+        if(forceEncod !=null){
+            forceEncoding = Boolean.parseBoolean(forceEncod);
+        }
+    }
+
+    public final void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (this.encoding != null &&
+                (this.forceEncoding || request.getCharacterEncoding() == null)) {
+            request.setCharacterEncoding(this.encoding);
+            if (this.forceEncoding) {
+                response.setCharacterEncoding(this.encoding);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    public void destroy() {
+
+    }
+}
