0519b31a0b82866c5db75b50ea30f2871703d48f
lexaz777 <vh1@mail.ru>
2018-08-12 23:32:51 +0300

    Добавлен новый функционал в Контроллеры

--- src/main/java/ru/zakharov/enterprise/controller/CategoryListController.java
+++ src/main/java/ru/zakharov/enterprise/controller/CategoryListController.java
@@ -1,4 +1,25 @@
 package ru.zakharov.enterprise.controller;
 
+import ru.zakharov.enterprise.dao.CategoryDAO;
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import java.util.List;
+
+@ViewScoped
+@ManagedBean
 public class CategoryListController {
+
+    @Inject
+    private CategoryDAO categoryDAO;
+
+    public List<String> getCategoriesNames() {
+        return categoryDAO.getCategoriesNames();
+    }
+
+    public List<String> getCategoriesIds() {
+        return categoryDAO.getCategoriesIds();
+    }
 }
--- src/main/java/ru/zakharov/enterprise/controller/ProductListController.java
+++ src/main/java/ru/zakharov/enterprise/controller/ProductListController.java
@@ -1,4 +1,27 @@
 package ru.zakharov.enterprise.controller;
 
+import ru.zakharov.enterprise.dao.ProductDAO;
+import ru.zakharov.enterprise.entity.Product;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import java.util.List;
+
+@ViewScoped
+@ManagedBean
 public class ProductListController {
+
+    @Inject
+    ProductDAO productDAO;
+
+
+
+    public List<Product> getProducts() {
+        return productDAO.getAllProducts();
+    }
+
+    public void removeProductById(String productId) {
+        productDAO.removeProductById(productId);
+    }
 }
